a8ea45a3f78265dd0c2c405de71a5ce98ef37cd4
Sebastian Franco Ulloa <sebastian.ulloa@xal.no>
2024-04-08 16:01:27 +0200

    tutorial 3

---
+++ 03tut/src/Content.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Content = () => {
+    const handleNameChange = () => {
+        const names = ['John', 'Jane', 'Doe'];
+        const int = Math.floor(Math.random() * names.length);
+        return names[int];
+    }
+    return (
+        <div>
+            <p>
+                Hello {handleNameChange()}
+            </p>
+
+        </div>
+    )
+}
+
+export default Content
